42a5f8b51dec00369daaf90ddcd600b1c3879451
Javier Verblud <javier.v@moni.com.ar>
2018-11-20 00:40:08 -0300

    minor advance over irc logic

--- ng-app/src/app/components/app/app.component.ts
+++ ng-app/src/app/components/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IRC } from '../../classes/irc';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private irc;
+
+
+  constructor() {
+    this.irc = new IRC();
+  }
 }
